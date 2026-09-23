6fbd5cf5b0e76e281a87aff3999074d07a0b70ba
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-09-23 13:59:25 +0600

    setting the namespace

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -28,6 +28,7 @@ const createUser = async (req: Request, res: Response) => {
 };
 
 const getAllUsers = async (req: Request, res: Response) => {
+  console.log(req.user)
   try {
     const result = await userService.getAllUsersFromDB();
 
